4829032df5d44032c5aadc08e945c72c2d617bdf
Billy Lee <billylee@billys-air.mynetworksettings.com>
2025-12-11 18:55:35 -0600

    finshed

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+let isModalOpen = false;
+let contrastToggle = false
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact(event) {
   event.preventDefault();
+  const loading = document.querySelector(`.modal__overlay--loading`);
+  const success = document.querySelector(`.modal__overlay--success`);
+  loading.classList += " modal__overlay--visible";
 
   emailjs
     .sendForm(
@@ -9,9 +38,24 @@ function contact(event) {
       'eeEiaje-L30e4GhrV'  // your public key
     )
     .then(() => {
-      console.log('this worked');
+     loading.classList.remove("modal__overlay--visible");
+     success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove ("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at billyylee76@gmail.com")
+
     })
-    .catch((error) => {
-      console.error('EmailJS error:', error);
-    });
 }
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
+
+
